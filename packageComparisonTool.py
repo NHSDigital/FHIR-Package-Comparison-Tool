12c272743225ed0f6757c23edc6df8d4b7a6d263
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2024-05-22 09:27:07 +0100

    Update packageComparisonTool.py

--- packageComparisonTool.py
+++ packageComparisonTool.py
@@ -71,9 +71,9 @@ def find_attributes(json_data, Type, attribute, attribute_dict=None, parent_keys
     if isinstance(json_data, dict):
         element = ''
         for key, value in json_data.items():
-            if key == 'path' and Type in value:
+            if key == 'id' and Type in value:
                 element = value
-            elif key == attribute and value !=ignore:
+            elif element != '' and key == attribute and value !=ignore:
                 attribute_path = f"{parent_keys}.{key}" if parent_keys else key
                 attribute_dict[element] = str(value)
             elif isinstance(value, (dict, list)):
